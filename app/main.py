7356183ae2e3093d6321d037c523b9105a18ac22
Dog2657 <reeceya2006@gmail.com>
2023-08-15 15:46:10 +0100

    Added section generation on startup

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
-
+from core import imager
+import config
+import math
+import os
 
 app = FastAPI()
 
 
 @app.on_event("startup")
 async def startup_event():
-    ...
+    if not os.path.isdir('sections'):
+        os.makedirs("sections")
+
+    section_width = math.floor(config.Canvas_Width / config.Canvas_Colums)
+    section_heigth = math.floor(config.Canvas_Height / config.Canvas_Rows)
+
+    for index in range(
+        config.Canvas_Colums * config.Canvas_Rows
+    ):
+        path = f'{os.getcwd()}/sections/{index}.png'
+
+        if not os.path.isfile(path):
+            img = imager.generateNewImage(section_width, section_heigth)
+            imager.save(path, img)
 
 
 @app.get("/config")
