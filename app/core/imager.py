8674c15b4a5024e8ec1c0d5e5b90b875aa03a0ee
Dog2657 <reeceya2006@gmail.com>
2023-08-15 17:58:07 +0100

    Fixed: Height and width being swaped

--- app/core/imager.py
+++ app/core/imager.py
@@ -5,7 +5,7 @@ def save(path: str, img: np.ndarray):
     cv2.imwrite(path, img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
 def generateBlankImage(width: int, height: int) -> np.ndarray:
-    img = np.ones((width, height, 3))
+    img = np.ones((height, width, 3))
     img = img * 255
     return img
 
