9ff96ce45430f911230c907d4e7215d166df9954
Dog2657 <reeceya2006@gmail.com>
2023-08-15 16:07:09 +0100

    Test image saveing

--- app/tests/test_imager.py
+++ app/tests/test_imager.py
@@ -1,11 +1,24 @@
-import os
 from ..core import imager
+import shutil
+import os
 
-def test_blank_image_generation():
+def test_image_generation():
     img = imager.generateBlankImage(300, 200)
 
-    assert img.shape == (300, 200, 3)
+    assert img.shape == (200, 300, 3)
 
     for x in range(300):
         for y in range(200):
-            assert list(img[x, y]) == [255, 255, 255]
+            assert list(img[y, x]) == [255, 255, 255]
+
+
+def test_image_saving():
+    os.makedirs(f'{os.getcwd()}/app/.temp/', exist_ok=True)
+    path = f'{os.getcwd()}/app/.temp/image.png'
+
+    img = imager.generateBlankImage(300, 200)
+    imager.save(path, img)
+
+    assert os.path.exists(path)
+
+    shutil.rmtree('/'.join(path.split('/')[0:-1]))
